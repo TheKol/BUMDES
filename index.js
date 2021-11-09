528cc0b478c9b56721d79a48d131fcd64f66e46d
R Indra Akbar S <rindraakbars@gmail.com>
2021-11-09 21:02:49 +0700

    minor change add plugins graphql

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ import express from 'express';
 import { typeDefs } from './data/schema';
 import { resolvers } from './data/resolvers';
 import { graphqlUploadExpress } from 'graphql-upload';
+import { ApolloServerPluginLandingPageGraphQLPlayground } from 'apollo-server-core';
 import cors from 'cors';
 import path from 'path';
 require('dotenv').config();
@@ -11,6 +12,7 @@ async function startServer() {
   const server = new ApolloServer({
     typeDefs,
     resolvers,
+    plugins: [ApolloServerPluginLandingPageGraphQLPlayground()],
     context: ({ req, res }) => ({ req, res }),
   });
 
